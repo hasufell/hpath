f5c541b9cc09c97a24b3e851592599df01028aec
Julian Ospald <hasufell@posteo.de>
2019-12-30 21:55:55 +0100

    Move doctests to shell script
    This fixes various problems with doctest:
    
    * https://github.com/sol/doctest/issues/245
    * https://github.com/composewell/streamly/issues/83
    
    Also clean up travis.yml for cabal-3.

---
+++ run-doctests.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -e
+
+if [ -n "${SKIP_DOCTESTS}" ] ; then
+    echo "Skipping doctests"
+    exit 0
+fi
+
+if ! command -v doctest >/dev/null ; then
+    tempdir="$(mktemp -d)"
+    (
+     cd "${tempdir}"
+     cabal install --installdir="${tempdir}" doctest
+    )
+    export PATH="${tempdir}:$PATH"
+fi
+
+set -x
+
+cabal exec doctest -- -isrc -XOverloadedStrings System.Posix.FilePath
+cabal exec doctest -- -isrc -XOverloadedStrings HPath
