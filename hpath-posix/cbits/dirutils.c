9b9a62337c4f2fb10ffd801fb30e5fc19d10c337
Julian Ospald <hasufell@posteo.de>
2020-04-14 18:09:10 +0200

    Be brave and don't wrap!

--- hpath-posix/cbits/dirutils.c
+++ hpath-posix/cbits/dirutils.c
@@ -19,23 +19,5 @@ unsigned int
 extern DIR*
 	__posixdir_fdopendir(int fd)
 {
-#ifdef __APPLE__
-	char fullpath[MAXPATHLEN];
-	DIR *d;
-
-	if(fcntl(fd, F_GETPATH, fullpath) < 0) {
-		perror("fcntl");
-		fprintf(stderr, "Unable to convert file descriptor back to pathname in __posixdir_fdopendir().\n");
-		return NULL;
-	}
-	if(close(fd) < 0) {
-		perror("close(fd) in __posixdir_fdopendir wrapper:");
-		return NULL;
-	}
-
-	d = opendir(fullpath);
-	return d;
-#else
 	return fdopendir(fd);
-#endif
 }
